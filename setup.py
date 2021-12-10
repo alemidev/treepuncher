32524780671394c096a06b4df1ae32480312f720
alemidev <me@alemi.dev>
2021-12-10 12:01:55 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
 	name='treepuncher',
-	version='0.0.1',
+	version='0.0.2',
 	description='An hackable Minecraft client, built with aiocraft',
 	url='https://github.com/alemidev/treepuncher',
 	author='alemi',
